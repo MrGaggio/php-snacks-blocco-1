2fb4e79055b8229b2b336238fc2764d1f51cb15b
Stefano Ruggiu <steruggiu@gmail.com>
2022-01-31 21:41:10 +0000

    snack 4 non va

---
+++ snack4.php
@@ -0,0 +1,36 @@
+<?php
+// Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
+
+$newArray = [];
+
+while($newArray < 15){
+    $number = rand(1, 20);
+    !in_array($number, $newArray);
+
+//     if (!in_array($number, $newArray)) {
+//         $newArray[] = $number;
+       
+//     }
+
+    var_dump($newArray);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+            var_dump($newArray)
+        ?>
+    </p>
+</body>
+</html>
